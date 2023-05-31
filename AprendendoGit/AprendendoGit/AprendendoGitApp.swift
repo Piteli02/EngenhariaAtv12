d2e6daedeb4afb77a6a87f16724b71fd8dd3977c
Caio Piteli <caiogpiteli@gmail.com>
2023-05-31 15:21:19 -0300

    Initial commit

---
+++ AprendendoGit/AprendendoGit/AprendendoGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AprendendoGitApp.swift
+//  AprendendoGit
+//
+//  Created by Caio Gomes Piteli on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AprendendoGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
